d7718b2991d31143cab6525daed23ee247fbff54
Rick Bacci <rickbacci@gmail.com>
2016-03-28 14:16:06 -0400

    refactor services.js

--- www/js/services.js
+++ www/js/services.js
@@ -4,27 +4,21 @@ angular.module('ideabox.services', [])
     .factory('IdeaFactory', function () {
         "use strict";
 
-        var idea = [],
-            ideaStore,
-            ideaSrv;
+        var idea, ideaStore, ideaSrv;
 
         ideaStore = localStorage.getItem("idea");
 
-        if (ideaStore !== null && ideaStore !== '' &&
-                angular.isArray(angular.fromJson(ideaStore))) {
+        if (ideaStore) {
             idea = angular.fromJson(ideaStore);
         }
+
         ideaSrv = {
             setIdeas: function (newIdea) {
-                idea = newIdea;
-                localStorage.setItem("idea", angular.toJson(idea));
+                localStorage.setItem("idea", angular.toJson(newIdea));
                 return true;
             },
             getIdeas: function () {
-                if (idea !== null) {
-                    return idea;
-                }
-                return [];
+                return idea !== null ? idea : [];
             }
         };
         return ideaSrv;
